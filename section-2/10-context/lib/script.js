a9cf96b9bbcfd26c03527b4524c83f4d42418be6
lawrencedovin <lawrence@qwikwire.com>
2020-08-09 21:48:15 -0400

    Added anonymous function section.

---
+++ section-2/10-context/lib/script.js
@@ -0,0 +1,9 @@
+let lawrence = {
+    firstName: "Lawrence",
+    lastName: "Dovin",
+    driveCar() {
+        console.log(this.firstName + " drives a car.");
+    }
+}
+
+lawrence.driveCar();
---
+++ section-2/11-anonymous-functions/lib/script.js
@@ -0,0 +1,5 @@
+// document.addEventListener('click', () => alert("Hello"))
+
+let myNumbers = [10, 500, 2000];
+let doubledNumbers = myNumbers.map((x) => x*2);
+console.log(doubledNumbers);
