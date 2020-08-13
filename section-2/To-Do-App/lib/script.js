498b8ee81fc6e0aff665f034048348bd917f8bd1
lawrencedovin <lawrence@qwikwire.com>
2020-08-12 22:51:43 -0400

    Completed To-Do App.

--- section-2/To-Do-App/lib/script.js
+++ section-2/To-Do-App/lib/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    let itemInput = document.querySelector("#itemInput");
     let toDoForm = document.querySelector("#toDoForm");
+    let itemInput = document.querySelector("#itemInput");
     let itemList = document.querySelector("#itemList");
 
     toDoForm.addEventListener('submit', (event) => {
@@ -8,12 +8,21 @@ window.onload = function() {
         // it is default behavior to refresh and reload the web page
         // then send that data off somewhere event.preventDefault() prevents that.
         createItem(itemInput.value);
-        itemInput.value = '';
     });
-
+    
     function createItem(item) {
-        let ourHTML = `<li>${item}<button>Delete</button></li>`;
+        let ourHTML = `<li>${item}<button onclick="this.parentElement.remove()">Delete</button></li>`;
         itemList.insertAdjacentHTML("beforeend", ourHTML);
         // 1st argument where do we want to add the new bit of content.
+        itemInput.value = '';
+        itemInput.focus();
     }
+
+    function deleteItem(itemToDelete) {
+        itemToDelete.parentElement.remove();
+    }
+    
 }
+
+
+// this.parentElement.remove()
