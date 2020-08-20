e30ee4205a972e76d49ca0da270908aae35d2ac5
lawrencedovin <lawrence@qwikwire.com>
2020-08-19 23:07:54 -0400

    Read data from database.

--- section-4/todo-app/server.js
+++ section-4/todo-app/server.js
@@ -13,8 +13,51 @@ mongodb.connect(connectionString, {useNewUrlParser: true}, (error, client) => {
 
 app.use(express.urlencoded({extended: false}));
 
+
 app.get('/', (request, response) => {
-    response.sendFile('index.html', {root: __dirname });
+    db.collection('items').find().toArray((error, items) => {
+        response.send(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Simple To-Do App</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+            </head>
+            <body>
+            <div class="container">
+                <h1 class="display-4 text-center py-1">To-Do App</h1>
+                
+                <div class="jumbotron p-3 shadow-sm">
+                <form action="/create-item" method="POST">
+                    <div class="d-flex align-items-center">
+                    <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                    <button class="btn btn-primary">Add New Item</button>
+                    </div>
+                </form>
+                </div>
+                
+                <ul class="list-group pb-5" id="itemList">
+                    ${items.map((item) => {
+                        return `
+                            <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                            <span class="item-text">${item.item}</span>
+                            <div>
+                                <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                                <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                            </div>
+                            </li>
+                        `;
+                    }).join("")}
+                </ul>
+                
+            </div>
+            
+            </body>
+            </html>
+        `);
+    });
 });
 
 app.post('/create-item', (request, response) => {
@@ -22,6 +65,6 @@ app.post('/create-item', (request, response) => {
     // db.collection('items').insertOne({name: "Meowsalot", species: "cat"})
     // MAKE NOTES WHEN YOU RETURN // 
     db.collection('items').insertOne({item: request.body.item}, () => {
-        response.send("Thanks for submitting");
+        response.redirect('/')
     })
 });
